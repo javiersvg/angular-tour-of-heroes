409494f7eb656223f2d9fd4fb8e2f9f72231bbe3
Javier Mino <javier.svg@gmail.com>
2018-04-09 10:52:04 -0500

    fix dashboard tests

--- src/app/dashboard/dashboard.component.spec.ts
+++ src/app/dashboard/dashboard.component.spec.ts
@@ -1,9 +1,25 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { APP_BASE_HREF } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 
 import { MatGridListModule } from '@angular/material/grid-list';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatListModule } from '@angular/material/list';
 
 import { DashboardComponent } from './dashboard.component';
 import { HeroSearchComponent } from '../hero-search/hero-search.component';
+import { AppRoutingModule } from '../app-routing.module';
+import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
+import { HeroesComponent } from '../heroes/heroes.component';
+
+import { SigninService } from '../signin.service';
+import { MessageService } from '../message.service';
+import { HeroService } from "../hero.service";
 
 describe('DashboardComponent', () => {
   let component: DashboardComponent;
@@ -11,8 +27,30 @@ describe('DashboardComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ DashboardComponent,HeroSearchComponent ],
-      imports: [ MatGridListModule ]
+      declarations: [ 
+        DashboardComponent,
+        HeroSearchComponent,
+        HeroDetailComponent,
+        HeroesComponent
+      ],
+      imports: [
+        MatGridListModule,
+        MatAutocompleteModule,
+        MatIconModule,
+        MatFormFieldModule,
+        MatDividerModule,
+        MatListModule,
+        AppRoutingModule,
+        ReactiveFormsModule,
+        FormsModule,
+        HttpClientModule,
+      ],
+      providers: [
+        { provide: APP_BASE_HREF, useValue : '/' },
+        SigninService,
+        MessageService,
+        HeroService
+      ],
     })
     .compileComponents();
   }));
